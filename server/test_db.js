e414cc3378b48fe98a909e7be7beb47a1d9593ea
Arnav Bule <arnav.bule05@gmail.com>
2026-02-26 19:54:46 +0530

    remove secrets and fix tests tsconfig

--- server/test_db.js
+++ server/test_db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 async function test() {
   console.log('Connecting to mongoose...');
-  await mongoose.connect('mongodb+srv://ddvgg:ddvgg@task-tracker.jmyqota.mongodb.net/mini-task-tracker?retryWrites=true&w=majority');
+  await mongoose.connect('mongodb+srv://{username}:{password}@task-tracker.jmyqota.mongodb.net/mini-task-tracker?retryWrites=true&w=majority');
   console.log('Connected! Finding workspaces...');
   try {
     const ws = await mongoose.connection.collection('workspaces').findOne({});
